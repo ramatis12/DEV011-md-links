6e34606cea775c40558d2e093f438c5d2a084956
Sara Ramirez <sara_ramatis@hotmail.com>
2023-12-05 17:58:48 -0600

    package

--- src/cli.js
+++ src/cli.js
@@ -5,10 +5,11 @@ const mdlFilePath = './README.md';
 const mdlFilePath1 = './README.png';
 const mdlFilePath2 = './thumb.png';
 const test = './test/array.md';
+
+const nombreDelArchivo = process.argv[2];
 const isValidateSelected = process.argv.find(element => element === "--validate") ? true : false; 
 const idStatsSelected = process.argv.find(element => element === "--stats") ? true : false;
-//console.log(mdLinks( mdlFilePath ));
 
-mdLinks(test, isValidateSelected, idStatsSelected).then ((res) => console.log(res))
+mdLinks(nombreDelArchivo, isValidateSelected, idStatsSelected).then ((res) => console.log(res))
 .catch((error) => console.log(error))
 
